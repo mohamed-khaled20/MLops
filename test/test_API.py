e1925451a3ea6fffe91f953804e269261e4f299f
Mohamed Khaled <33982050+mohamed-khaled20@users.noreply.github.com>
2022-07-06 22:46:56 +0200

    Update test_API.py

--- test/test_API.py
+++ test/test_API.py
@@ -12,6 +12,13 @@ def test_alive():
   res = requests.get('https://khaled-covid19-mlops.herokuapp.com/alive')
   res_code = str(res.status_code)
   assert(res_code == "200")
+  
+def test_predict():
+  img = open("NORMAL.png","rb")
+  d   = {'file':img}
+  res = requests.post('https://khaled-covid19-mlops.herokuapp.com/prediction', files=d)
+  res_code = str(res.status_code)
+  assert(res_code == "200")
 
 
 
