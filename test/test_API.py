f873a849c7e432497126d363a5a79f9ed3b09cc7
Mohamed Khaled <33982050+mohamed-khaled20@users.noreply.github.com>
2022-07-06 21:52:01 +0200

    Create test_API.py

---
+++ test/test_API.py
@@ -0,0 +1,22 @@
+import flask
+import requests
+import json
+import os
+import magic
+from flask import abort,jsonify
+from predict import predict
+from werkzeug.exceptions import HTTPException
+from gevent.pywsgi import WSGIServer
+from ... import app
+
+
+app = app.app
+app.run(debug=True)
+
+def test_alive():
+  res = requests.get('0.0.0.0:5000')
+  res_code = str(res).split('[')[1].split(']')[0]
+  assert(res_code == 200)
+
+
+
