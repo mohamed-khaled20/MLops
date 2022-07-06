152c03133653ce68062fb92a22e102f7a87d8c23
Mohamed Khaled <33982050+mohamed-khaled20@users.noreply.github.com>
2022-07-06 22:26:59 +0200

    Update test_API.py

--- test/test_API.py
+++ test/test_API.py
@@ -9,7 +9,7 @@ from gevent.pywsgi import WSGIServer
 import sys
 
 def test_alive():
-  res = requests.get('https //127.0.0.1:5000')
+  res = requests.get('https://127.0.0.1:5000')
   res_code = str(res).split('[')[1].split(']')[0]
   assert(res_code == 200)
 
