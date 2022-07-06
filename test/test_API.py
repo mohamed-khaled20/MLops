532b25c19c3322d513aaf64dd6c929cc6aef7614
Mohamed Khaled <33982050+mohamed-khaled20@users.noreply.github.com>
2022-07-06 22:14:02 +0200

    Update test_API.py

--- test/test_API.py
+++ test/test_API.py
@@ -8,11 +8,6 @@ from werkzeug.exceptions import HTTPException
 from gevent.pywsgi import WSGIServer
 import sys
 
-os.chdir("/home/runner/work/MLops/MLops")
-from app import app
-
-app.run(debug=True)
-
 def test_alive():
   res = requests.get('0.0.0.0:5000')
   res_code = str(res).split('[')[1].split(']')[0]
